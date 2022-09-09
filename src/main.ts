0e79b515e2ee09b0214f36fa61be0a437c3b9700
Eike  Lurz <eike.lurz@sportalliance.com>
2022-09-09 20:36:51 +0200

    ✨ Add action logic (#1)

---
+++ src/main.ts
@@ -0,0 +1,101 @@
+import { setFailed, getInput, addPath, info, debug, error } from '@actions/core'
+import { find, downloadTool, extractTar, cacheFile } from '@actions/tool-cache'
+import { HttpClient } from '@actions/http-client'
+import { exec } from '@actions/exec'
+import { platform, arch } from 'os'
+import { chmodSync } from 'fs'
+
+const LATEST_RELEASE_URL = 'https://api.github.com/repos/cli/cli/releases/latest'
+
+const determineArch = (): string => {
+  switch (arch()) {
+    case 'arm64':
+      return 'arm64'
+    case 'x64':
+      return 'amd64'
+    default:
+      throw new Error(`Not supported arch '${arch()}'`)
+  }
+}
+
+const buildDirName = (version: string): string => `gh_${version}_${platform()}_${determineArch()}`
+
+const buildDownloadUrl = (version: string): string =>
+  `https://github.com/cli/cli/releases/download/v${version}/${buildDirName(version)}.tar.gz`
+
+const determineLatestVersion = async (): Promise<string> => {
+  debug('Fetching latest release version...')
+  const response = await new HttpClient('fetch-release').getJson(LATEST_RELEASE_URL)
+  debug(`Got ${response}`)
+
+  if (response.statusCode !== 200) {
+    setFailed('Failed fetching latest release version')
+  }
+  const latestTag = (response.result as { tag_name: string }).tag_name
+  debug(`Latest version is ${latestTag}`)
+
+  return latestTag.replace('v', '')
+}
+
+const authenticate = async (): Promise<void> => {
+  const token = getInput('token', { required: true })
+  let infoOutput = ''
+  let errorOutput = ''
+
+  await exec('gh', ['auth', 'login', '--with-token'], {
+    listeners: {
+      stdout: (data: Buffer) => {
+        infoOutput += data.toString()
+      },
+      stderr: (data: Buffer) => {
+        errorOutput += data.toString()
+      },
+    },
+    input: Buffer.from(token),
+  })
+
+  if (errorOutput.length > 0) {
+    error(errorOutput)
+  }
+  if (infoOutput.length > 0) {
+    info(infoOutput)
+  }
+}
+
+const run = async (): Promise<void> => {
+  const version = getInput('version') || (await determineLatestVersion())
+  const currentArch = determineArch()
+  const cached = find('gh-cli', version, currentArch)
+
+  if (!cached) {
+    info(`Did not find cached binaries. Downloading...`)
+    const downloadUrl = buildDownloadUrl(version)
+    const ghCliTar = await downloadTool(downloadUrl, 'gh-cli')
+    debug(`Downloaded to ${ghCliTar}`)
+
+    chmodSync(ghCliTar, '775')
+    const extracted = await extractTar(ghCliTar)
+    debug(`Extracted to ${extracted}`)
+
+    const ghCliPath = await cacheFile(
+      `${extracted}/${buildDirName(version)}/bin/gh`,
+      'gh',
+      'gh-cli',
+      version,
+      currentArch
+    )
+
+    addPath(ghCliPath)
+  } else {
+    info(`Found cached binaries for 'gh_${version}_${platform()}_${currentArch}'`)
+    addPath(cached)
+  }
+  info('Github CLI was added to the PATH')
+
+  await authenticate()
+  info('The CLI is authenticated with the token')
+}
+
+debug('Staring Action...')
+run().catch((err) => setFailed(err.message))
+debug('Done')
